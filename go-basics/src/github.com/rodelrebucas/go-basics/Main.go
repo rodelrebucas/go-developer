107c4a124805b26d09cd19d08552b5e373e041ce
rodel <rodel.rebucas@copyleft.no>
2020-02-10 22:07:46 +0800

    go routines start

--- go-basics/src/github.com/rodelrebucas/go-basics/Main.go
+++ go-basics/src/github.com/rodelrebucas/go-basics/Main.go
@@ -3,47 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	// A. Creating empty interface
-	var myObj writerCloser = myWriterCloser{}
-	fmt.Println(myObj) // {}
-
+	fmt.Println("start goroutines")
 }
-
-// interfaces
-type writer interface {
-	Write([]byte) (int, error)
-}
-
-type closer interface {
-	Close() error
-}
-
-type writerCloser interface {
-	writer
-	closer
-}
-
-// combined interfaces
-type myWriterCloser struct{}
-
-// implement the write method
-func (wc myWriterCloser) Write(data []byte) (int, error) {
-	return 0, nil
-}
-
-// implement the close method
-func (wc myWriterCloser) Close() error {
-	return nil
-}
-
-// with close()
-/** Output
-I am tes
-ting buf
-fers
-*/
-
-// without close()
-/** I am tes
-ting buf
-*/
