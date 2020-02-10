4f7615c808eb6e21efda9aa6e56396edcd8348ab
rodel <rodelrebucas@gmail.com>
2020-02-10 14:27:30 +0800

    interface

--- go-basics/src/github.com/rodelrebucas/go-basics/Main.go
+++ go-basics/src/github.com/rodelrebucas/go-basics/Main.go
@@ -1,25 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
-	// create variable of type Writer
-	var w Writer = ConsoleWriter{}
-	w.Write([]byte("Hello Go!"))
+	// A. Creating interface with more than one methods
+	var wc writerCloser = newBufferedWriterCloser() // Call the constsructor
+	wc.Write([]byte("I am testing buffers"))
+	wc.Close()
+	// wc.buffer is not accessible
+	// writerCLoser is not aware of the implementation of bufferedWriterCloser
+
+	// type conversion with error handling to avoid panic
+	bwc, ok := wc.(*bufferedWriterCloser)
+	if ok {
+		fmt.Println("Buffer: ", bwc.buffer) // buffer is now accessible
+	} else {
+		fmt.Println("Convertion failed")
+	}
+
 }
 
-// Interface - collection of function signatures; describes *behavior* unlike *struct* which which describes data
-// Naming convention should be suffix with *er* with single method
-type Writer interface {
-	// describe the method signature
+// interfaces
+type writer interface {
 	Write([]byte) (int, error)
 }
 
-// Implicitly implements the Writer interface
-type ConsoleWriter struct{}
-
-// Create the method signature *Write*
-func (vw ConsoleWriter) Write(data []byte) (int, error) {
-	n, err := fmt.Println(string(data))
-	return n, err
+type closer interface {
+	Close() error
 }
+
+// combined interfaces
+type writerCloser interface {
+	writer
+	closer
+}
+
+type bufferedWriterCloser struct {
+	buffer *bytes.Buffer
+}
+
+// implement the write method
+func (bwc *bufferedWriterCloser) Write(data []byte) (int, error) {
+	n, err := bwc.buffer.Write(data)
+	if err != nil {
+		return 0, err
+	}
+
+	v := make([]byte, 8)
+	// read and print only 8 characters
+	for bwc.buffer.Len() > 8 {
+		_, err := bwc.buffer.Read(v)
+		if err != nil {
+			return 0, err
+		}
+		_, err = fmt.Println(string(v))
+		if err != nil {
+			return 0, err
+		}
+	}
+	return n, nil
+}
+
+// implement the close method
+func (bwc *bufferedWriterCloser) Close() error {
+	// read the next n bytes from the buffer
+	// else return all bytes if n < buffer length
+	for bwc.buffer.Len() > 0 {
+		data := bwc.buffer.Next(8)
+		_, err := fmt.Println(string(data))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// constructor
+func newBufferedWriterCloser() *bufferedWriterCloser {
+	return &bufferedWriterCloser{
+		buffer: bytes.NewBuffer([]byte{}),
+	}
+}
+
+// with close()
+/** Output
+I am tes
+ting buf
+fers
+*/
+
+// without close()
+/** I am tes
+ting buf
+*/
